9fcabb89848c546f4aa5d55eeefad475ea80c147
Raj <p.rajendarreddy@gmail.com>
2024-10-19 13:43:16 +0100

    Add project files.

---
+++ ConvertHtmlToPdf/Program.cs
@@ -0,0 +1,20 @@
+﻿using iText.Html2pdf;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string html = "<html><body><h1>HTML to Pdf</h1><div>This is an example to convert Html to Pdf file using itext7 in C#</div></body></html>";
+        string outputPath = "C:\\Pdf\\output.pdf";
+
+       
+
+        using (FileStream outputStream = new FileStream(outputPath, FileMode.Create))
+        {
+            ConverterProperties converterProperties = new ConverterProperties();
+            HtmlConverter.ConvertToPdf(html, outputStream, converterProperties);
+        }
+
+        Console.WriteLine("HTML to PDF conversion completed successfully and pdf file create at C:\\Pdf.");
+    }
+}
